0722407cf0bc5bb7aa2582fd07a892fd64f15d79
Colin Huber <cbhuber@gmail.com>
2023-03-21 22:33:09 -0600

    test: slugify to console.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 // --------------------------------------------------------------------------------
@@ -21,6 +22,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 // --------------------------------------------------------------------------------
 
