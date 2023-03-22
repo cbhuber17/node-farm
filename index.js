5827c137a96f98cd784623bc95fe3e02b81e6f2f
Colin Huber <cbhuber@gmail.com>
2023-03-21 22:02:14 -0600

    feat: product description using URL parser and exporting module.

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
-// Server
-// temp for "template"
+// --------------------------------------------------------------------------------
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // /{%PRODUCTNAME%}/g is a regex that replaces globally
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
-
+// Get the HTML from the text files
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -33,14 +18,17 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+// --------------------------------------------------------------------------------
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -48,15 +36,17 @@ const server = http.createServer((req, res) => {
       .join(""); // Join all elements to create HTML string
 
     const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
-
     res.end(output);
 
     // Product page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" }); // Headers must be provided prior to sending API data
     res.end(data);
 
@@ -70,6 +60,8 @@ const server = http.createServer((req, res) => {
     res.end("<h1>Page not found!</h1>");
   }
 });
+
+// --------------------------------------------------------------------------------
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
