e37aebd1b2c7347a07ba93369e6a1a1bd69193fe
Colin Huber <cbhuber@gmail.com>
2023-09-20 19:34:56 -0600

    fix: change port to 10000 for onrender hosting.:

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ const server = http.createServer((req, res) => {
 
 // --------------------------------------------------------------------------------
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+server.listen(10000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 10000");
 });
