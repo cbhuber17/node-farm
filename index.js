af3ea92cf48a9c2e88496a8350e96b1853ae6108
Colin Huber <cbhuber@gmail.com>
2023-09-20 19:36:52 -0600

    fix: bind host to 0.0.0.0 for onrender.

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ const server = http.createServer((req, res) => {
 
 // --------------------------------------------------------------------------------
 
-server.listen(10000, "127.0.0.1", () => {
+server.listen(10000, "0.0.0.0", () => {
   console.log("Listening to requests on port 10000");
 });
